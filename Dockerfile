bed2b1d0d805883ffe998f88b21adf23e2ea1a2b
bassistjune <bassistjune@gmail.com>
2023-07-20 05:45:28 +0900

    #_Qoddi_배포를 위한 Gradle 배포에러수정

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-# Base 이미지 설정
-FROM openjdk:17-jdk
-
-# 애플리케이션 파일을 Docker 이미지로 복사
-COPY build/libs/vue-backend-0.0.1-SNAPSHOT.jar /app.jar
-
-# 컨테이너에서 실행될 명령어 설정
-CMD ["java", "-jar", "app.jar"]
--- src/main/java/com/example/vuebackend/multipart/MultipartConfig.java
+++ src/main/java/com/example/vuebackend/multipart/MultipartConfig.java
@@ -1,14 +1,21 @@
-package multipart;
+package com.example.vuebackend.multipart;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 @Configuration
 public class MultipartConfig {
+    @Value("${file.upload-dir}")
+    private String uploadDir;
     @Bean
     public CommonsMultipartResolver multipartResolver() {
         CommonsMultipartResolver resolver = new CommonsMultipartResolver();
         resolver.setMaxUploadSize(10 * 1024 * 1024);  // 업로드할 파일의 최대 크기 설정
         return resolver;
+    }
+    @Bean
+    public String uploadDir() {
+        return uploadDir;
     }
 }
