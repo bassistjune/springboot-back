40ef238ce8f66a6d6226f5eb5de4a7590fd51d42
bassistjune <bassistjune@gmail.com>
2023-07-23 00:36:25 +0900

    #_Cors설정

---
+++ src/main/java/com/example/vuebackend/CorsConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.vuebackend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("https://bassistjune.github.io")
+                // 이 설정은 https://sub.example.app 인 origin을 허용합니다.
+                ;
+            }
+        };
+    }
+}
